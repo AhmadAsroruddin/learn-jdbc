51839c884e3e0a4677ca4958be471645d37fdf1b
Ahmad Asroruddin <asroruddinahmad09@gmail.com>
2024-02-21 06:38:36 +0700

    batch processing

--- src/test/java/ari/first/jdbc/ConnectionPoolTest.java
+++ src/test/java/ari/first/jdbc/ConnectionPoolTest.java
@@ -3,19 +3,46 @@ package ari.first.jdbc;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import first.Util;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.extension.TestWatcher;
 
 import java.sql.*;
 
 public class ConnectionPoolTest {
+    private static Connection conn;
 
-    @BeforeAll
-    static void hikariCPConnectionTest(){
+    @BeforeEach
+    void setUp()throws Exception{
+        conn = Util.getDataSource().getConnection();
+
+        conn.setAutoCommit(false);
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException{
+        try {
+            // Commit the transaction if all tests passed
+            conn.commit();
+        } catch (Exception e){
+            conn.rollback();
+            throw e;
+        }
+        finally {
+            // Close the connection
+            conn.close();
+        }
+    }
+    @Test
+    void hikariCPConnectionTest(){
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl("jdbc:postgresql://localhost:5432/mydatabase?serverTimeZone=Asia/Jakarta");
         config.setUsername("postgres");
-        config.setPassword("Handball123");
+        config.setPassword("Hkkii23");
 
         config.setMaximumPoolSize(10);
         config.setMinimumIdle(5);
@@ -24,7 +51,7 @@ public class ConnectionPoolTest {
 
         try {
             HikariDataSource dataSource = new HikariDataSource();
-            Connection conn = dataSource.getConnection();
+            conn = dataSource.getConnection();
             conn.close();
             dataSource.close();
         } catch (Exception e){
@@ -39,45 +66,137 @@ public class ConnectionPoolTest {
     }
 
     @Test
-    void insertDateTest() throws SQLException {
-        Connection conn = Util.getDataSource().getConnection();
-        Statement statement = conn.createStatement();
+    void updateTest(){
+        PreparedStatement preparedStatement = null;
 
-        String query = """
-                    INSERT INTO dummy_table(name, birth_date) values('Ahmad', '2001-08-17')
+        try{
+            String query = """
+                    update dummy_table set name = ? where id = ?
                 """;
-        statement.executeUpdate(query);
+            preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1,"AIUSRA");
+            preparedStatement.setInt(2, 14);
 
-        statement.close();
-        conn.close();
+            preparedStatement.executeUpdate();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    void insertDateTest() throws SQLException {
+        PreparedStatement preparedStatement = null;
+        try {
+            String query = """
+                    INSERT INTO dummy_table(name, birth_date) values(?, ?)
+                """;
+            preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, "KoaSla");
+            preparedStatement.setDate(2, Date.valueOf("2022-09-19"));
+
+            preparedStatement.executeUpdate();
+        }catch (Exception e){
+            throw e;
+        }
+
+    }
+    @Test
+    void transaction(){
+
+        try{
+            conn.setAutoCommit(false);
+            insertDateTest();
+            delete();
+
+            conn.commit();
+        } catch (SQLException e) {
+            if(conn != null){
+                try{
+                    System.out.println("Transaction Being Roll Back");
+                    conn.rollback();
+                }catch (Exception ex){
+                    System.out.println(ex);
+                }
+            }
+        }
+    }
+
+    @Test
+    void delete() throws SQLException {
+        PreparedStatement preparedStatement = null;
+        String query = """
+                    delete from dummy_table where id=?
+                """;
+        preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setInt(1, 12);
+
+        preparedStatement.executeUpdate();
+
     }
 
     @Test
     void showTableData() throws SQLException {
-        Connection conn = Util.getDataSource().getConnection();
-        Statement statement = conn.createStatement();
         PreparedStatement preparedStatement = null;
+        try {
 
-        String query = """
+
+            String query = """
                     SELECT * from dummy_table where id = ?
                 """;
 
-        preparedStatement = conn.prepareStatement(query);
-        preparedStatement.setInt(1,6);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        ResultSetMetaData metaData = preparedStatement.getMetaData();
-        int column = metaData.getColumnCount();
+            preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setInt(1, 13);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            ResultSetMetaData metaData = preparedStatement.getMetaData();
+            int column = metaData.getColumnCount();
 
-        for(int i = 1; i<=column; i++){
-            System.out.printf("%-20s", metaData.getColumnName(i));
-        }
-        System.out.println();
-        while(resultSet.next()){
-            for(int i =1; i<=column; i++){
-                System.out.printf("%-20s", resultSet.getString(i));
-
+            for (int i = 1; i <= column; i++) {
+                System.out.printf("%-20s", metaData.getColumnName(i));
             }
             System.out.println();
+            while (resultSet.next()) {
+                for (int i = 1; i <= column; i++) {
+                    System.out.printf("%-20s", resultSet.getString(i));
+
+                }
+                System.out.println();
+            }
+        }catch (Exception e){
+            throw e;
+        }
+    }
+
+    @RegisterExtension
+    static TestWatcher watchman = new TestWatcher() {
+        @Override
+        public void testFailed(ExtensionContext context, Throwable cause) {
+            // Rollback the transaction if any test fails
+            ConnectionPoolTest testInstance = (ConnectionPoolTest) context.getTestInstance().get();
+            try {
+                testInstance.conn.rollback();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    @Test
+    void insertBatchData(){
+        PreparedStatement preparedStatement = null;
+        try {
+            String query = """
+                    INSERT INTO dummy_table(name, birth_date) values(?, ?)
+                """;
+            preparedStatement = conn.prepareStatement(query);
+            for(int i = 1; i <= 1000; i++){
+                preparedStatement.setString(1, "Koala "+i);
+                preparedStatement.setDate(2, Date.valueOf("2022-09-19"));
+                preparedStatement.addBatch();
+            }
+
+            preparedStatement.executeBatch();
+        }catch (Exception e){
+            System.out.println(e);
         }
     }
 }
